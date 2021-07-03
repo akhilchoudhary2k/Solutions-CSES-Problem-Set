5a14356f99c5bd17d835e0de754d1fdad85e0451
Akhil Choudhary <akhil.choudhary2000@gmail.com>
2021-07-04 01:33:57 +0530

    add new files

---
+++ high_score.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+
+void test_case(){
+	int n,m;
+	cin >> n >> m;
+
+
+	// dijkstra
+
+	// rep n-1 times:
+	// 	u <-- min distance unvisited node
+	// 	for v in nbrs of u:
+	// 		update distance of v 
+
+	
+
+}
+
+signed main(){
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	
+	int t=1;
+	// cin >> t;
+
+	while(t--) test_case();
+	
+	return 0;
+}
